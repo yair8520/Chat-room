1439e5e4357588a351d216028514fbe3e7fc713a
YAIR GABAY <yair8520@gmail.com>
2021-06-06 13:26:12 +0300

    fgjh

---
+++ src/main/java/controllers/SessionListener.java
@@ -0,0 +1,36 @@
+/*
+package controllers;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import java.util.concurrent.atomic.AtomicInteger;
+
+*/
+/**
+ * a @WebListener class for session count
+ *//*
+
+@WebListener
+public class SessionListener implements HttpSessionListener {
+    private final AtomicInteger activeSessions;
+
+    public SessionListener() {
+        super();
+        activeSessions = new AtomicInteger();
+    }
+
+    public int getTotalActiveSession() {
+        return activeSessions.get();
+    }
+
+    public void sessionCreated(final HttpSessionEvent event) {
+        activeSessions.incrementAndGet();
+        System.out.println("SessionListener +++ Total active session are " + activeSessions.get());
+    }
+    public void sessionDestroyed(final HttpSessionEvent event) {
+        activeSessions.decrementAndGet();
+        System.out.println("SessionListener --- Total active session are " + activeSessions.get());
+    }
+}
+*/
