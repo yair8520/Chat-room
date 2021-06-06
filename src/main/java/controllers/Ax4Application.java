511e622cbf62306cf8b591f562516f7074ec96ff
YAIR GABAY <yair8520@gmail.com>
2021-06-06 13:25:09 +0300

    fgjh

--- src/main/java/controllers/Ax4Application.java
+++ src/main/java/controllers/Ax4Application.java
@@ -2,8 +2,11 @@ package controllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @SpringBootApplication
+@EnableJdbcHttpSession
 public class Ax4Application {
 
     public static void main(String[] args) {
--- src/main/java/controllers/BeanConfiguration.java
+++ src/main/java/controllers/BeanConfiguration.java
@@ -1,10 +1,13 @@
 package controllers;
 
 
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.web.context.annotation.SessionScope;
+
+import javax.servlet.http.HttpSessionListener;
 
 /**
  * create some beans witn various scopes using QUALIFIERS (method names)
@@ -16,7 +19,17 @@ public class BeanConfiguration {
     @Bean
     @SessionScope
     public UserData  sessionScopeId() {
-
         return new UserData();
     }
+    /*@Bean
+    public ServletListenerRegistrationBean<SessionListener> sessionListenerWithMetrics() {
+        ServletListenerRegistrationBean<SessionListener> listenerRegBean = new ServletListenerRegistrationBean<>();
+
+        listenerRegBean.setListener(new SessionListener());
+        return listenerRegBean;
+    }*/
+   /* @Bean
+    public ServletListenerRegistrationBean<HttpSessionListener> sessionListener() {
+        return new ServletListenerRegistrationBean<HttpSessionListener>(new SessionListener());
+    }*/
 }
