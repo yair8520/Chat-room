9a938da5ef2bec621c3ffe687a837b62b654274a
YAIR GABAY <yair8520@gmail.com>
2021-06-13 12:22:47 +0300

    commit

--- src/main/java/com/FiltersConfig.java
+++ src/main/java/com/FiltersConfig.java
@@ -1,27 +1,27 @@
-//package com;
-//
-//
-//import com.controllers.ChatInterceptor;
-//import org.springframework.context.annotation.Configuration;
-//
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-///*
-//    EXAMPLE: CONTROLLING THE STATIC FOLDER
-//
-//  here we register our interceptor class and the session listener
-//  WebMvcConfigurer allows configuring all of the MVC:
-// */
-//@EnableWebMvc
-//@Configuration
-//public class FiltersConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry){
-//        // if you want to apply filter only for REST controller: change the "/**" pattern
-//       registry.addInterceptor(new ChatInterceptor()).addPathPatterns("/chat");
-//    }
-//
-//}
+package com;
+
+
+import com.controllers.ChatInterceptor;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/*
+    EXAMPLE: CONTROLLING THE STATIC FOLDER
+
+  here we register our interceptor class and the session listener
+  WebMvcConfigurer allows configuring all of the MVC:
+ */
+@EnableWebMvc
+@Configuration
+public class FiltersConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        // if you want to apply filter only for REST controller: change the "/**" pattern
+       registry.addInterceptor(new ChatInterceptor()).addPathPatterns("/chat");
+    }
+
+}
